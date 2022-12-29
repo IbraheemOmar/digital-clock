b4f241bd5053897c8b4b63a6e2d0f9f9f40b9d61
ibruh555 <103038658+ibruh555@users.noreply.github.com>
2022-12-29 09:27:09 -0500

    Add files via upload

--- code.js
+++ code.js
@@ -1,1 +1,28 @@
+const hourEl = document.querySelector("#hour")
+const minutesEl= document.querySelector("#minutes")
+const secondsEl = document.querySelector("#seconds")
+const ampmEl = document.querySelector("#ampm")
 
+function digitalClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM"
+    
+    if(h>12){
+        h= h-12
+        ampm = "PM"
+
+    }
+
+    hourEl.textContent = h;
+    minutesEl.innerText = m;
+    secondsEl.innerText = s;
+    ampmEl.innerHTML = ampm
+
+    setTimeout(()=>{
+        digitalClock();
+    },1000)
+}
+
+digitalClock()
